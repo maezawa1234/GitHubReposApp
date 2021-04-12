dc16aa406c52789e366e96a74a1a50a3c1beb36e
Maezawa <chokowa177@gmail.com>
2021-04-12 18:39:35 +0900

    WebView画面にprogressバーを実装

--- GitHubReposApp/RepositoryDetail/RepositoryDetailViewController.swift
+++ GitHubReposApp/RepositoryDetail/RepositoryDetailViewController.swift
@@ -1,10 +1,15 @@
 import UIKit
 import WebKit
+import RxSwift
+import RxCocoa
+
 
 class RepositoryDetailViewController: UIViewController {
+    @IBOutlet weak var progressView: UIProgressView!
     @IBOutlet weak var webView: WKWebView!
     
     private let repository: Repository
+    private let disposeBag = DisposeBag()
     
     init?(coder: NSCoder, repository: Repository) {
         self.repository = repository
@@ -20,11 +25,27 @@ class RepositoryDetailViewController: UIViewController {
         super.viewDidLoad()
  
         setup()
+        binding()
     }
     
     private func setup() {
         self.navigationItem.title = repository.name
         let request = URLRequest(url: repository.htmlURL)
         webView.load(request)
+    }
+    
+    private func binding() {
+        webView.rx.observe(Double.self, #keyPath(WKWebView.estimatedProgress))
+            .flatMap { estimatedProgress -> Observable<Double> in
+                estimatedProgress.map(Observable.just) ?? .empty()
+            }
+            .bind(to: Binder(self) { me, progress in
+                UIView.animate(withDuration: 0.3) {
+                    let isShown = 0.0..<1.0 ~= progress
+                    me.progressView.alpha = isShown ? 1 : 0
+                    me.progressView.setProgress(Float(progress), animated: isShown)
+                }
+            })
+            .disposed(by: disposeBag)
     }
 }
